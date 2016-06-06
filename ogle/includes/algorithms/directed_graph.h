4cce69864138ff56a82fb1a90207eb2bed7cadb7
David McLaren <damlaren@gmail.com>
2016-06-05 18:56:05 -0700

    Added comments to DirectedGraph.

--- ogle/includes/algorithms/directed_graph.h
+++ ogle/includes/algorithms/directed_graph.h
@@ -24,39 +24,81 @@ namespace ogle {
 
 /**
  * @brief A directed graph.
+ *
+ * KeyType is used to uniquely identify nodes. ValueType describes the data
+ * stored in each node.
  */
 template<typename KeyType, typename ValueType>
 class DirectedGraph {
  public:
+  /**
+   * @brief A node in the graph.
+   *
+   * Identified by a unique key kept by the graph.
+   */
   class Node {
    public:
     friend class DirectedGraph;
 
+    /**
+     * @brief Constructor.
+     * @param value Value to store in node.
+     */
     explicit Node(const ValueType& value)
       : value_(value) {
     }
 
+    /**
+     * @brief Accessor.
+     * @return See #neighbors_.
+     */
     const stl_vector<Node*>& neighbors() const {
       return neighbors_;
     }
 
+    /**
+     * @brief Accessor.
+     * @return See #back_pointers_.
+     */
     const stl_vector<Node*>& back_pointers() const {
       return back_pointers_;
     }
 
+    /// Value to store in node.
     ValueType value_;
 
    private:
+    /// Nodes this node has an edge to.
     stl_vector<Node*> neighbors_;
+
+    /// Nodes with an edge back to this node.
     stl_vector<Node*> back_pointers_;
   };
 
+  /**
+   * @brief Function type to check if node matches search criteria.
+   */
   using MatchFunction = bool(const Node* node);
 
+  /**
+   * @brief Add a node to graph without any edges.
+   * @param key Unique key to identify new node.
+   * @param value Value to store in node.
+   * @return true if node was inserted, false if key is duplicate.
+   */
   const bool AddNode(const KeyType& key, const ValueType& value) {
     return nodes_.emplace(key, Node(value)).second;
   }
 
+  /**
+   * @brief Add a directed edge to the graph.
+   *
+   * Both nodes must exist, and the edge must not always exist.
+   *
+   * @param src_key Identifies source node on edge.
+   * @param dest_key Identifies destination node on edge.
+   * @return true if edge was added successfully.
+   */
   const bool AddEdge(const KeyType& src_key, const KeyType& dest_key) {
     auto src_it = nodes_.find(src_key);
     if (src_it == nodes_.end()) {
@@ -86,6 +128,12 @@ class DirectedGraph {
     return true;
   }
 
+  /**
+   * @brief Get value from node.
+   * @param key Identifies node to get value from.
+   * @return Pair containing (1) Retrieved value, and (2) flag indicating
+   *         whether node was found.
+   */
   const std::pair<const ValueType&, bool> GetValue(const KeyType& key) const {
     const auto it = nodes_.find(key);
     if (it != nodes_.end()) {
@@ -94,6 +142,12 @@ class DirectedGraph {
     return {{}, false};
   }
 
+  /**
+   * @brief Get neighbors that a node has edges to.
+   * @param key Identifies node to query.
+   * @return Pair containing (1) Neighboring nodes, and (2) flag indicating
+   *         whether node with this key was found.
+   */
   const std::pair<stl_vector<Node*>, bool> GetNeighbors(
       const KeyType& key) const {
     const auto node_it = nodes_.find(key);
@@ -103,6 +157,11 @@ class DirectedGraph {
     return {{}, false};
   }
 
+  /**
+   * @brief Remove a node and all connecting edges from the graph.
+   * @param key Identifies node to remove.
+   * @return Flag indicating whether node was removed.
+   */
   const bool Remove(const KeyType& key) {
     auto node_it = nodes_.find(key);
     if (node_it != nodes_.end()) {
@@ -138,6 +197,11 @@ class DirectedGraph {
     return nodes_.size();
   }
 
+  /**
+   * @brief Retrieves nodes matching a condition.
+   * @param match_function A #MatchFunction to test conditions on a Node.
+   * @return Keys and values for all nodes matching the condition.
+   */
   stl_vector<std::pair<const KeyType&, ValueType*>> GetMatches(
       MatchFunction match_function) {
     stl_vector<std::pair<const KeyType&, ValueType*>> results;
@@ -150,6 +214,7 @@ class DirectedGraph {
   }
 
  private:
+  /// All nodes in graph, keyed by unique ID.
   stl_unordered_map<KeyType, Node> nodes_;
 };
 
